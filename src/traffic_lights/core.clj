d6c1fec2a7ea047a09dcb3208145f2746aa71306
Michael Drogalis <mjd3089@rit.edu>
2013-06-23 20:22:26 -0400

    Initial commit.

---
+++ src/traffic_lights/core.clj
@@ -0,0 +1,6 @@
+(ns traffic-lights.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/traffic_lights/core_test.clj
@@ -0,0 +1,7 @@
+(ns traffic-lights.core-test
+  (:require [clojure.test :refer :all]
+            [traffic-lights.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
