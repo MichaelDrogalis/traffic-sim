e05c3f421296ca6774b40a755521eb7bd1705b08
Michael Drogalis <mjd3089@rit.edu>
2013-06-25 21:14:28 -0400

    Full light construction.

--- src/traffic_lights/core.clj
+++ src/traffic_lights/core.clj
@@ -1,9 +1,5 @@
-(ns traffic-lights.core)
-
-(def light-schema {"North Street" {:states [:red :yellow :green] :init [:red]}
-                   "South Street" {:states [:red :yellow :green] :init [:red]}
-                   "East Street"  {:states [:red :yellow :green] :init [:red]}
-                   "West Street"  {:states [:red :yellow :green] :init [:red]}})
+(ns traffic-lights.core
+  (:require [dire.core :refer [with-precondition!]]))
 
 (defn legal-init-state? [states init-vals]
   (every? (fn [x] (some #{x} states)) init-vals))
@@ -11,3 +7,13 @@
 (defn construct-light-face [street states init-vals]
   {street (merge (zipmap states (repeat false))
                  (zipmap init-vals (repeat true)))})
+
+(with-precondition! #'construct-light-face
+  :valid-init-vals
+  (fn [_ states init-vals]
+    (legal-init-state? states init-vals)))
+
+(defn construct-light [schema]
+  (reduce (fn [result [street {:keys [states init]}]]
+            (conj result (construct-light-face street states init)))
+          {} schema))
--- test/traffic_lights/core_test.clj
+++ test/traffic_lights/core_test.clj
@@ -9,3 +9,15 @@
 (fact (construct-light-face "North Street" [:red :yellow :green] [:red])
       => {"North Street" {:red true :yellow false :green false}})
 
+(fact (construct-light-face "North Street" [:red :yellow :green] [:blue])
+      => (throws clojure.lang.ExceptionInfo))
+
+(fact (construct-light {"North Street" {:states [:red :yellow :green] :init [:red]}
+                        "South Street" {:states [:red :yellow :green] :init [:red]}
+                        "East Street"  {:states [:red :yellow :green] :init [:red]}
+                        "West Street"  {:states [:red :yellow :green] :init [:red]}})
+      => {"North Street" {:red true :yellow false :green false}
+          "South Street" {:red true :yellow false :green false}
+          "East Street"  {:red true :yellow false :green false}
+          "West Street"  {:red true :yellow false :green false}})
+
