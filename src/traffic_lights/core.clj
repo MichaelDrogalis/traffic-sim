8675ad3732cb94bc6820f7d3f40c12b269adeeb0
Michael Drogalis <mjd3089@rit.edu>
2013-07-20 21:10:02 -0400

    Simulate light progressing.

--- src/traffic_lights/core.clj
+++ src/traffic_lights/core.clj
@@ -16,6 +16,9 @@
 (defn build-light-set-catalog [schema]
   (catalog schema :light-set/ident))
 
+(defn build-light-map [lights]
+  (apply merge (map (fn [[v light]] {v (:light/init light)}) lights)))
+
 (defn find-light [light-catalog ident]
   (first (filter (fn [x] (= (:light/ident x) ident)) light-catalog)))
 
@@ -35,7 +38,7 @@
   (map (fn [{:keys [states] :as step}]
          (assoc step :states
                 (merge (map (fn [[variable lights]]
-                              {(substitutions variable) lights})
+                              {:var-origin variable :sub {(substitutions variable) lights}})
                             states))))
        schedule))
 
@@ -45,9 +48,28 @@
         schedule (substitute-lights-in-schedule (:light-set/schedule schedule) lights)]
     (assoc light-set :light-set/substitute lights :light-set/schedule schedule)))
 
-(let [lc (build-light-catalog intersection-schema)
-      sc (build-schedule-catalog intersection-schema)
-      ls (build-light-set-catalog intersection-schema)]
-  (doseq [x ls]
-    (pprint (construct-light lc sc x))))
+(defn compile-lights []
+  (let [lc (build-light-catalog intersection-schema)
+        sc (build-schedule-catalog intersection-schema)
+        ls (build-light-set-catalog intersection-schema)]
+    (map
+     (fn [x]
+       (let [light-gen (construct-light lc sc x)
+             light-map (build-light-map (:light-set/substitute light-gen))]
+         {:light-gen light-gen :light-map light-map}))
+     ls)))
+
+(def my-light-spec (first (compile-lights)))
+
+(def my-light-schedule (:light-gen my-light-spec))
+
+(def my-light-agent (agent (:light-map my-light-spec)))
+
+(doseq [{:keys [states duration]} (-> my-light-schedule :light-set/schedule)]
+  (send my-light-agent
+        (fn [my-light]
+          (apply merge my-light (map (fn [{:keys [var-origin sub]}]
+                              {var-origin (first (vals sub))})
+                            states))))
+  (Thread/sleep duration))
 
