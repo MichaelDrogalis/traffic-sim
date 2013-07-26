74ba47778a8b80dd9b1c08202c5c2f1e1e7bf0f5
Michael Drogalis <mjd3089@rit.edu>
2013-07-26 18:49:40 -0400

    Light working.

--- src/traffic_lights/core.clj
+++ src/traffic_lights/core.clj
@@ -18,40 +18,29 @@
 (defn build-schedule-catalog [schema]
   (build-catalog schema :schedule/ident))
 
-(defn build-combo-catalog [schema]
-  (build-catalog schema :light-combo/ident))
-
 (def light-catalog (build-light-catalog intersection-schema))
 
 (def schedule-catalog (build-schedule-catalog intersection-schema))
 
-(def combo-catalog (build-combo-catalog intersection-schema))
+(defn build-light-for-schedule [schedule light-catalog]
+  (fmap #(:light-face/init (light-catalog %)) (:schedule/substitute schedule)))
 
-(defn combo-resting-state [substitions light-catalog]
-  (fmap #(:light-face/init (light-catalog %)) substitions))
-
-(defn combo-schedule [schedule schedule-catalog]
-  (:schedule/sequence (schedule-catalog schedule)))
-
-(defn turn-combo-on [combo schedule]
+(defn turn-light-on [traffic-light schedule light-catalog]
   (doseq [step schedule]
     (let [{:keys [states duration]} step]
-      (send combo (fn [light] (apply merge light states)))
+      (send traffic-light (fn [light] (apply merge light states)))
       (Thread/sleep duration))))
 
 ;;; Begin experimentation.
 
-(def combo (:standard-light-combo combo-catalog))
+(def schedule (schedule-catalog :shamrock-schedule))
 
-(def initial-lights (combo-resting-state (:light-combo/substitute combo) light-catalog))
-
-(def schedule (combo-schedule (:light-combo/schedule combo) schedule-catalog))
-
-(def traffic-light (agent initial-lights))
+(def traffic-light
+  (agent (build-light-for-schedule schedule light-catalog)))
 
 (add-watch traffic-light :printer
-             (fn [_ _ _ light]
-               (info light)))
+           (fn [_ _ _ light]
+             (info light)))
 
 (defn turn-on-light [light schedule]
   (doseq [{:keys [states duration] :as step} schedule]
@@ -59,5 +48,5 @@
     (Thread/sleep duration)))
 
 (defn -main [& args]
-  (turn-on-light traffic-light schedule))
+  (turn-on-light traffic-light (:schedule/sequence schedule)))
 
