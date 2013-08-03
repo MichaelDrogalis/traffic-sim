644833d7503d8af7fef3d04e8f2d0f6c85059441
Michael Drogalis <mjd3089@rit.edu>
2013-08-03 13:29:53 -0400

    No wait on intx.

--- src/traffic_lights/core.clj
+++ src/traffic_lights/core.clj
@@ -158,9 +158,7 @@
 
 (defn drive-through-intersection [me]
   (info (:id @me) "is driving through the intersection.")
-  (dosync
-   (alter (intx-area-index (:intersection/of (:src @me))) conj me))
-  (Thread/sleep 2000)
+  (dosync (alter (intx-area-index (:intersection/of (:src @me))) conj me))
   (dosync
    (alter (intx-area-index (:intersection/of (:src @me))) (partial filter (partial not= me)))
    (alter (queues-index (:src @me)) #(vec (filter (partial not= me) %)))
