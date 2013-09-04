9a0feba2eb1ff3c9fe6343156e1b9825cfe3991d
Michael Drogalis <mjd3089@rit.edu>
2013-09-03 20:38:22 -0400

    Automatically funnel ingress->egress.

--- src/traffic_lights/core.clj
+++ src/traffic_lights/core.clj
@@ -4,29 +4,38 @@
             [traffic-lights.queue :as q]
             [traffic-lights.index :as i]))
 
-(defn parallel-map-merge [f state]
-  (apply merge (pmap (fn [[k v]] {k (f v)}) state)))
+(defn par-map-merge [f state & args]
+  (apply merge (map (fn [[k v]] {k (apply f k v args)}) state)))
 
-(defn build-light-state-machine [x]
+(defn build-light-state-machine [_ x]
   {:state (:state-diff (first x))
    :fns (mapcat q/light-transition->fns x)})
 
 (def light-state-machines
-  (parallel-map-merge build-light-state-machine i/traffic-light-index))
+  (par-map-merge build-light-state-machine i/traffic-light-index))
 
 (defn loud-lights! [lights]
-  (pprint (parallel-map-merge :state lights)))
+  (pprint (par-map-merge :state lights)))
 
 (defn loud-lanes! [lanes]
-  (pprint (parallel-map-merge :state lanes)))
+  (pprint (par-map-merge :state lanes)))
+
+(defn transform-lanes [old-lanes]
+  (par-map-merge q/harvest-lane
+                 (par-map-merge q/advance-cars-in-lane
+                                (par-map-merge q/mark-ripe
+                                               (par-map-merge q/ch->lane old-lanes)))
+                 i/directions-index i/lane-index))
 
 (defn genesis! [old-lanes old-lights]
-  (let [new-lanes  (future (-> old-lanes
-                               (partial parallel-map-merge q/ch->lane)
-                               (partial parallel-map-merge q/mark-ripe)
-                               (partial parallel-map-merge q/advance-cars-in-lane)
-                               (partial parallel-map-merge q/harvest-lane)))
-        new-lights (future (parallel-map-merge q/next-light-state old-lights))]
-    (Thread/sleep 1000)
-    (recur @new-lanes @new-lights)))
+  (pprint old-lanes)
+  (let [new-lanes (transform-lanes old-lanes)
+        new-lights (par-map-merge q/next-light-state old-lights)]
+    (Thread/sleep 2000)
+    (recur new-lanes new-lights)))
+
+(def car {:id "Mike" :len 5 :buf 3})
+(q/enqueue-into-ch (:channel (second (first i/lane-state-index))) car)
+
+(genesis! i/lane-state-index [])
 
--- src/traffic_lights/queue.clj
+++ src/traffic_lights/queue.clj
@@ -29,9 +29,6 @@
       (let [target (nth old (dec my-slot))]
         (conj new-lane (drive-watching-forward car target speed))))))
 
-(defn advance-cars-in-lane [{:keys [state] :as lane}]
-  (assoc lane :state (r/reduce (partial advance 1 state) [] state)))
-
 (defn next-light-state [{:keys [state fns]}]
   (let [[f & more] fns]
     {:state (f state) :fns (conj (vec more) f)}))
@@ -47,17 +44,26 @@
   (assert (<= (.size channel) 1))
   (.take channel))
 
-(defn ch->lane [{:keys [channel state] :as lane}]
+(defn advance-cars-in-lane [_ {:keys [state] :as lane}]
+  (assoc lane :state (r/reduce (partial advance 1 state) [] state)))
+
+(defn ch->lane [_ {:keys [channel state] :as lane}]
   (if-not (zero? (.size channel))
     (add-to-lane lane (take-from-channel channel))
     lane))
 
-(defn mark-ripe [{:keys [channel state] :as lane}]
+(defn mark-ripe [_ {:keys [state] :as lane}]
   (let [[head-car & more] state]
     (if head-car
       (assoc lane :state (conj more (assoc head-car :ripe? (zero? (:front head-car)))))
       lane)))
 
-(defn harvest-lane [lane]
-  lane)
+(defn harvest-lane [lane-id {:keys [state] :as lane} directions-index lane-index]
+  (let [[head-car & more] state]
+    (if (:ripe? head-car)
+      (let [out-lane (get (:directions (get directions-index (:id head-car))) lane-id)
+            out-ch (:channel (get lane-index out-lane))]
+        (enqueue-into-ch out-ch (dissoc head-car :ripe?))
+        (assoc lane :state (or more [])))
+      lane)))
 
