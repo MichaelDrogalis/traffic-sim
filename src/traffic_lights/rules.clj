eda8b89b8c10b1c5a3c545f11d12fb193667930f
Michael Drogalis <mjd3089@rit.edu>
2013-09-05 18:55:13 -0400

    Fix the bad code.

--- src/traffic_lights/rules.clj
+++ src/traffic_lights/rules.clj
@@ -9,25 +9,30 @@
     :dst (mapping dst)
     :yield (map (fn [[src dst]] [(first (mapping src)) (first (mapping dst))]) yield)))
 
-;;; 1. Obtain all lane data
-;;; 2. Get the rule set name for the lane
-;;; 3. Get all of the registered rules for the rule set
-;;; 4. Get the intersection-var map
-;;; 5. Evaluate the lane's local substitution map
-;;; 6. Evaluate each registered rule's substitution map
-;;; 7. Evalulate each atomic rule's src, dst, and yield
-(defn eval-all-atomic-rules [lane-index sub-index atomic-index lane-id]
-  (let [lane (get lane-index lane-id)
-        rule-set (:street.lane.install/rules lane)
-        var-mapping (i/lane-var-catalog (:intersection/of lane))
-        registered-rules (get sub-index rule-set)
-        evaluated-lane-var-mapping (fmap #(var-mapping %) (:street.lane.install/substitute lane))
-        evaluated-registered-rules (map #(assoc % :lane.rules/substitute
-                                                (merge (:lane.rules/substitute %) evaluated-lane-var-mapping))
-                                        registered-rules)
-        evaled-atomic-rules (map #(eval-atom (atomic-index (:lane.rules/register %))
-                                                    (:lane.rules/substitute %)) evaluated-registered-rules)]
-    evaled-atomic-rules))
+(defn local-var-index [lane var-catalog]
+  (var-catalog (:intersection/of lane)))
 
-(pprint (eval-all-atomic-rules i/lane-index i/lanes-rules-subtitution-index i/atomic-rule-index (ffirst i/lane-index)))
+(defn rule-set-name [lane]
+  (:street.lane.install/rules lane))
+
+(defn eval-local-lane-subs [lane locals]
+  (fmap #(locals %) (:street.lane.install/substitute lane)))
+
+(defn eval-binders [registered-rules evaled-lane-subs]
+  (map #(assoc % :lane.rules/substitute
+               (merge (:lane.rules/substitute %) evaled-lane-subs))
+       registered-rules))
+
+(defn eval-atomic-rule [atomic-index evaled-binders]
+  (map #(eval-atom (atomic-index (:lane.rules/register %))
+                   (:lane.rules/substitute %)) evaled-binders))
+
+(defn eval-all-atomic-rules [lane sub-index atomic-index]
+  (let [locals-index (local-var-index lane i/lane-var-catalog)
+        rule-set (rule-set-name lane)
+        registered-rules (get sub-index rule-set)
+        evaled-lane-subs (eval-local-lane-subs lane locals-index)
+        evaled-binders (eval-binders registered-rules evaled-lane-subs)
+        evaled-atomic-rules (eval-atomic-rule atomic-index evaled-binders)]
+    evaled-atomic-rules))
 
