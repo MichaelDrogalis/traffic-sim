329c8b92a4ac5e118b20778f0815d4deef42dd67
Michael Drogalis <mjd3089@rit.edu>
2013-09-07 20:25:43 -0400

    Fix transform.

--- src/traffic_lights/core.clj
+++ src/traffic_lights/core.clj
@@ -5,9 +5,6 @@
             [traffic-lights.index :as i]
             [traffic-lights.rules :as r]))
 
-(defn par-map-merge [f state & args]
-  (apply merge (pmap (fn [[k v]] {k (apply f k v args)}) state)))
-
 (defn build-light-state-machine [x]
   {:state (:state-diff (first (:state-seq x)))
    :fns (mapcat q/light-transition->fns (:state-seq x))})
@@ -16,18 +13,18 @@
   (map build-light-state-machine i/traffic-light-catalog))
 
 (defn transform-ingress-lanes [old-lanes safety-fn]
-  (par-map-merge q/harvest-ingress-lane
-                 (par-map-merge q/advance-cars-in-lane
-                                (par-map-merge q/mark-ripe
-                                               (par-map-merge q/ch->lane old-lanes)))
-                 i/directions-index i/ingress-lane-state-index safety-fn))
+  (map q/harvest-ingress-lane
+       (map q/advance-cars-in-lane
+            (map q/mark-ripe
+                 (map q/ch->lane old-lanes)))
+       i/directions-index i/ingress-lane-state-catalog safety-fn))
 
 (defn transform-egress-lanes [old-lanes]
-  (par-map-merge q/harvest-egress-lane
-                 (par-map-merge q/advance-cars-in-lane
-                                (par-map-merge q/mark-ripe
-                                               (par-map-merge q/ch->lane old-lanes)))
-                 i/directions-index i/egress-lane-state-index))
+  (map q/harvest-egress-lane
+       (map q/advance-cars-in-lane
+            (map q/mark-ripe
+                 (map q/ch->lane old-lanes)))
+       i/directions-index i/egress-lane-state-catalog))
 
 (defn genesis! [old-i-lanes old-e-lanes old-lights safety-fn]
   (let [new-i-lanes (future (transform-ingress-lanes old-i-lanes (partial safety-fn old-i-lanes old-lights)))
