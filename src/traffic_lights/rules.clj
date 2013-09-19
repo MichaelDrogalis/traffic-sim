a3d818985f069b106b21e65ee90e7dd7bf54ed90
Michael Drogalis <mjd3089@rit.edu>
2013-09-19 18:47:39 -0400

    Pull apart safety.

--- src/traffic_lights/rules.clj
+++ src/traffic_lights/rules.clj
@@ -22,17 +22,25 @@
 (defn matches-yield? [lane-idx rule]
   (every? true? (map (partial apply lane-clear? lane-idx) (:yield rule))))
 
+(defn index-quads [lanes]
+  (into {} (map u/index-by-quad lanes)))
+
+(defn all-rules-for-src [storage src]
+  (let [lane-idx (index-quads (p/lanes storage))]
+    (p/resolve-rules storage (lane-idx (quad src)))))
+
+(defn light-for-src [storage old-lights src]
+  (let [lane-idx (index-quads (p/lanes storage))
+        face (:street.lane.install/light (lane-idx (quad src)))]
+    (getx (:state (old-lights (:intersection/of src))) face)))
+
 (defn safe-to-go? [storage]
   (fn [old-lanes old-lights src dst]
-    (let [lane-id (quad src)
-          lane-idx (into {} (map u/index-by-quad (p/lanes storage)))
-          intx (:intersection/of lane-id)
-          face (:street.lane.install/light (lane-idx lane-id))
-          light (getx (:state (old-lights intx)) face)
-          rules (p/resolve-rules storage (lane-idx lane-id))
+    (let [all-rules (all-rules-for-src storage src)
+          light (light-for-src storage old-lights src)
           match-f (every-pred (partial matches-src? src)
                               (partial matches-dst? dst)
                               (partial matches-light? light)
                               (partial matches-yield? old-lanes))]
-      (not (empty? (filter match-f rules))))))
+      (not (empty? (filter match-f all-rules))))))
 
